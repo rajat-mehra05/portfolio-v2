d02470aec5134891b4a8dfa0a9181646b36482ed
rajat-mehra05 <rajat.m.5278@gmail.com>
2026-01-26 13:51:50 +0530

    refactor: remove redundant closing remarks from blog posts

--- layouts/blog.tsx
+++ layouts/blog.tsx
@@ -49,7 +49,7 @@ export default function BlogLayout({
         <div className="w-full mt-4 prose dark:prose-dark max-w-none">
           {children}
         </div>
-        <div className="text-sm mt-10 text-gray-700 dark:text-gray-300">
+        {/* <div className="text-sm mt-10 text-gray-700 dark:text-gray-300">
           <a
             href={discussUrl(post.slug)}
             target="_blank"
@@ -65,7 +65,7 @@ export default function BlogLayout({
           >
             {'Edit on GitHub'}
           </a>
-        </div>
+        </div> */}
       </article>
     </Container>
   );
