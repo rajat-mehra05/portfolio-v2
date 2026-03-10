624631d20c3cf33d4cb2fccc11e2110275c6650f
rajat-mehra05 <rajat.m.5278@gmail.com>
2026-03-10 21:53:49 +0530

    style: enhance text color consistency

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -47,7 +47,7 @@ export default function Footer(props: Props) {
 
       <div className="w-full max-w-2xl grid grid-cols-1 gap-4 pb-16 sm:grid-cols-2">
         <div className="flex flex-col space-y-4">
-          <Link href="/" className="text-gray-400 hover:text-gray-500 transition">
+          <Link href="/" className="text-gray-500 hover:text-gray-600 transition">
             Home
           </Link>
         </div>
